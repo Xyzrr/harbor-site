c4e6e4b095755aeba111d6f5e006424f0c8b7718
John Qian <johnlongqian@gmail.com>
2021-05-27 14:46:46 -0700

    appease nextjs

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,5 +1,56 @@
-import * as S from "./download.styles";
 import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+
+export const Wrapper = styled.div`
+  background: rgba(26, 26, 26);
+  min-height: 100vh;
+  padding: 16px;
+`;
+
+export const Container = styled.div`
+  max-width: 1080px;
+  margin: 0 auto 0;
+  padding: 20vh 16px 16px;
+`;
+
+export const HeaderLogo = styled(Image)`
+  opacity: 0.7;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 56px;
+  margin-bottom: 52px;
+  color: white;
+  font-weight: 600;
+`;
+
+export const PlatformLogoWrapper = styled.div`
+  filter: invert(100%);
+  margin-right: 16px;
+  margin-left: -4px;
+`;
+
+export const PlatformLogo = styled(Image)``;
+
+export const DownloadButton = styled.a`
+  display: flex;
+  width: fit-content;
+  align-items: center;
+  padding: 16px 32px;
+  border-radius: 8px;
+  color: black;
+  text-decoration: none;
+  font-size: 26px;
+  font-weight: 600;
+  background: white;
+  &:hover {
+    background: #f6f6f6;
+  }
+`;
 
 export interface DownloadPageProps {
   className?: string;
@@ -7,32 +58,32 @@ export interface DownloadPageProps {
 
 const DownloadPage: React.FC<DownloadPageProps> = ({ className }) => {
   return (
-    <S.Wrapper className={className}>
-      <S.Container>
+    <Wrapper className={className}>
+      <Container>
         <a href="/">
-          <S.HeaderLogo
+          <HeaderLogo
             src="/logo_light_colored.svg"
             alt="Harbor logo"
             width={311 / 2}
             height={63 / 2}
           />
         </a>
-        <S.Title>Download Harbor</S.Title>
-        <S.DownloadButton
+        <Title>Download Harbor</Title>
+        <DownloadButton
           href="https://github.com/Xyzrr/harbor/releases/download/v0.3.0/Harbor-0.3.0.dmg"
           download
         >
-          <S.PlatformLogoWrapper>
-            <S.PlatformLogo
+          <PlatformLogoWrapper>
+            <PlatformLogo
               src="/platform-logo-macos.svg"
               width={26}
               height={26}
             />
-          </S.PlatformLogoWrapper>
+          </PlatformLogoWrapper>
           Download for MacOS
-        </S.DownloadButton>
-      </S.Container>
-    </S.Wrapper>
+        </DownloadButton>
+      </Container>
+    </Wrapper>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,224 @@
-import * as S from "./index.styles";
+import styled, { keyframes } from "styled-components";
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import HeroVisual from "../components/HeroVisual";
+
+export const Wrapper = styled.div`
+  background: rgba(26, 26, 26);
+  min-height: 100vh;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  position: sticky;
+  top: 0;
+  padding: 16px;
+  justify-content: space-between;
+  background: rgba(26, 26, 26, 0.5);
+  backdrop-filter: blur(16px);
+  z-index: 1;
+`;
+
+export const HeaderLogo = styled(Image)`
+  opacity: 0.7;
+`;
+
+export const HeaderCTA = styled.a`
+  color: white;
+  font-weight: 600;
+  letter-spacing: 2px;
+`;
+
+export const Main = styled.main`
+  margin: 0 auto;
+`;
+
+export const HeroWrapper = styled.div`
+  padding: 0 16px;
+`;
+
+export const HeroContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1080px;
+  margin: 64px auto;
+  align-items: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin-top: 0;
+  }
+`;
+
+export const HeroText = styled.div`
+  width: 600px;
+  padding: 16px;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const HeroTitle = styled.h1`
+  color: white;
+  font-size: 96px;
+  margin-bottom: 18px;
+  background: linear-gradient(to right, #7cd8ff, #b2e6fd);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  @media (max-width: 768px) {
+    font-size: 64px;
+  }
+`;
+
+export const HeroSubTitle = styled.h2`
+  color: white;
+  font-size: 48px;
+  font-weight: 400;
+  @media (max-width: 768px) {
+    font-size: 32px;
+    margin-bottom: 64px;
+  }
+`;
+
+export const StyledHeroVisual = styled(HeroVisual)`
+  margin-right: 16px;
+`;
+
+export const IntroWrapper = styled.div`
+  padding: 0 16px;
+`;
+
+export const IntroContainer = styled.div`
+  max-width: 1080px;
+  margin: 0 auto;
+`;
+
+export const IntroText = styled.div`
+  padding: 16px;
+`;
+
+export const IntroImageWrapper = styled.div`
+  height: 400px;
+  margin-left: -32px;
+  margin-right: -32px;
+  overflow: hidden;
+`;
+
+export const IntroTitle = styled.h2`
+  color: white;
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 8px;
+`;
+
+export const IntroDescription = styled.p`
+  margin-top: 0;
+  color: #ccc;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+const scrollSignalAnimation = keyframes`
+  0%   {
+      opacity: 0;
+      transform: translateY(-20px);
+  }
+  30%  {
+      opacity: 0.6;
+      transform: translateY(-8px);
+  }
+  60%  {
+    opacity: 0.6;
+    transform: translateY(8px);
+}
+  100% {
+      opacity: 0;
+      transform: translateY(20px);
+  }
+`;
+
+export const ScrollSignalWrapper = styled.div`
+  margin-top: 56px;
+  animation: ${scrollSignalAnimation} 3s linear 0s infinite;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const ScrollSignal = styled(Image)``;
+
+export const FeaturesWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+`;
+
+export const FeaturesContainer = styled.div`
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 64px 0;
+`;
+
+export const FeaturesTitle = styled.h2`
+  padding: 16px;
+  font-size: 32px;
+`;
+
+export const FeaturesList = styled.div`
+  display: flex;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+  }
+`;
+
+export const FeatureWrapper = styled.div`
+  padding: 16px;
+  width: 270px;
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+export const FeatureIcon = styled.div`
+  width: 50px;
+  height: 50px;
+  background: #888;
+`;
+
+export const FeatureTitle = styled.h3`
+  font-size: 15px;
+  font-weight: 600;
+`;
+
+export const FeatureDescription = styled.p`
+  font-size: 15px;
+  color: #888;
+`;
+
+export const FinalCTAWrapper = styled.div`
+  height: 400px;
+`;
+
+export const Footer = styled.footer`
+  background: #555;
+  padding: 0 16px;
+`;
+
+export const FooterContainer = styled.div`
+  max-width: 1080px;
+  padding: 64px 16px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterText = styled.div`
+  color: #ccc;
+  font-size: 14px;
+`;
 
 const Home: React.FC = () => {
   return (
@@ -20,108 +237,108 @@ const Home: React.FC = () => {
           rel="stylesheet"
         ></link>
       </Head>
-      <S.Wrapper>
-        <S.Header>
-          <S.HeaderLogo
+      <Wrapper>
+        <Header>
+          <HeaderLogo
             src="/logo_white.svg"
             alt="Harbor logo"
             width={311 / 3}
             height={63 / 3}
           />
-          <S.HeaderCTA href="download">Get started</S.HeaderCTA>
-        </S.Header>
+          <HeaderCTA href="download">Get started</HeaderCTA>
+        </Header>
 
-        <S.Main>
-          <S.HeroWrapper>
-            <S.HeroContainer>
-              <S.HeroText>
-                <S.HeroTitle>Be with your team.</S.HeroTitle>
-                <S.HeroSubTitle>From anywhere.</S.HeroSubTitle>
-                <S.ScrollSignalWrapper>
-                  <S.ScrollSignal
+        <Main>
+          <HeroWrapper>
+            <HeroContainer>
+              <HeroText>
+                <HeroTitle>Be with your team.</HeroTitle>
+                <HeroSubTitle>From anywhere.</HeroSubTitle>
+                <ScrollSignalWrapper>
+                  <ScrollSignal
                     src="/scroll_signal.svg"
                     alt="Scroll signal"
                     width={87}
                     height={44}
                   />
-                </S.ScrollSignalWrapper>
-              </S.HeroText>
-              <S.StyledHeroVisual />
-            </S.HeroContainer>
-          </S.HeroWrapper>
-          <S.IntroWrapper>
-            <S.IntroContainer>
-              <S.IntroText>
-                <S.IntroTitle>A virtual space in your menu bar</S.IntroTitle>
-                <S.IntroDescription>
+                </ScrollSignalWrapper>
+              </HeroText>
+              <StyledHeroVisual />
+            </HeroContainer>
+          </HeroWrapper>
+          <IntroWrapper>
+            <IntroContainer>
+              <IntroText>
+                <IntroTitle>A virtual space in your menu bar</IntroTitle>
+                <IntroDescription>
                   Harbor is designed to be as small as possible, both in screen
                   space and CPU footprint. <br />
                   So it's comfortable enough to leave on all day while you work.
-                </S.IntroDescription>
-              </S.IntroText>
-              <S.IntroImageWrapper>
+                </IntroDescription>
+              </IntroText>
+              <IntroImageWrapper>
                 <Image src="/desktop.png" width={2560} height={1532} />
-              </S.IntroImageWrapper>
-            </S.IntroContainer>
-          </S.IntroWrapper>
+              </IntroImageWrapper>
+            </IntroContainer>
+          </IntroWrapper>
 
-          <S.FeaturesWrapper>
-            <S.FeaturesContainer>
-              <S.FeaturesTitle>Built for productivity</S.FeaturesTitle>
-              <S.FeaturesList>
-                <S.FeatureWrapper>
+          <FeaturesWrapper>
+            <FeaturesContainer>
+              <FeaturesTitle>Built for productivity</FeaturesTitle>
+              <FeaturesList>
+                <FeatureWrapper>
                   <Image src="/screenshare.svg" width={50} height={50} />
-                  <S.FeatureTitle>Interactive screen sharing</S.FeatureTitle>
-                  <S.FeatureDescription>
+                  <FeatureTitle>Interactive screen sharing</FeatureTitle>
+                  <FeatureDescription>
                     See each other's cursors while screen sharing, like pointing
                     with fingers in real life.
-                  </S.FeatureDescription>
-                </S.FeatureWrapper>
-                <S.FeatureWrapper>
+                  </FeatureDescription>
+                </FeatureWrapper>
+                <FeatureWrapper>
                   <Image src="/whisper.svg" width={50} height={50} />
 
-                  <S.FeatureTitle>Directed conversations</S.FeatureTitle>
-                  <S.FeatureDescription>
+                  <FeatureTitle>Directed conversations</FeatureTitle>
+                  <FeatureDescription>
                     Whisper to an individual without leaving the group. Good for
                     pulling someone aside or asking a question without
-                    disturbing the others.
-                  </S.FeatureDescription>
-                </S.FeatureWrapper>
-                <S.FeatureWrapper>
-                  <Image src="/focus.svg" width={50} height={50} />
+                    disturbing the other
+                  </FeatureDescription>
+                </FeatureWrapper>
+                <FeatureWrapper>
+                  <Image src="/focusvg" width={50} height={50} />
 
-                  <S.FeatureTitle>Focus mode</S.FeatureTitle>
-                  <S.FeatureDescription>
+                  <FeatureTitle>Focus mode</FeatureTitle>
+                  <FeatureDescription>
                     Disable communications on a timer when you need to crank
                     something out in silence.
-                  </S.FeatureDescription>
-                </S.FeatureWrapper>
-                <S.FeatureWrapper>
+                  </FeatureDescription>
+                </FeatureWrapper>
+                <FeatureWrapper>
                   <Image src="/pulse.svg" width={50} height={50} />
 
-                  <S.FeatureTitle>A team pulse</S.FeatureTitle>
-                  <S.FeatureDescription>
+                  <FeatureTitle>A team pulse</FeatureTitle>
+                  <FeatureDescription>
                     See what work apps your teammates are on, to get a better
                     sense of when you can interrupt.
-                  </S.FeatureDescription>
-                </S.FeatureWrapper>
-              </S.FeaturesList>
-            </S.FeaturesContainer>
-          </S.FeaturesWrapper>
-          <S.FinalCTAWrapper></S.FinalCTAWrapper>
-        </S.Main>
-        <S.Footer>
-          <S.FooterContainer>
-            <S.HeaderLogo
+                  </FeatureDescription>
+                </FeatureWrapper>
+              </FeaturesList>
+            </FeaturesContainer>
+          </FeaturesWrapper>
+          <FinalCTAWrapper></FinalCTAWrapper>
+        </Main>
+        <Footer>
+          <FooterContainer>
+            <HeaderLogo
               src="/logo_white.svg"
               alt="Harbor"
               width={311 / 3}
               height={63 / 3}
             />
-            <S.FooterText>©{new Date().getFullYear()} Harbor</S.FooterText>
-          </S.FooterContainer>
-        </S.Footer>
-      </S.Wrapper>
+            <FooterText>©{new Date().getFullYear()} Harbor</FooterText>
+          </FooterContainer>
+        </Footer>
+      </Wrapper>
     </>
   );
 };
